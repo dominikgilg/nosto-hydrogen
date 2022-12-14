e3347d97b41905a2076881e137aaf6bad1a109bc
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-12-14 10:42:02 +0100

    fixed config

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,3 @@
-// import react from "@vitejs/plugin-react";
-// import { resolve } from "path";
-// import { defineConfig } from "vite";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     lib: {
-//       entry: resolve(__dirname, "src/index.client.js"),
-//       name: "@nosto/nosto-hydrogen",
-//       formats: ["es", "umd"],
-//       fileName: (format) => `index.${format}.client.js`,
-//     },
-//     rollupOptions: {
-//       external: ["react", "react-dom", "@shopify/hydrogen"],
-//       output: {
-//         globals: {
-//           react: "React",
-//           "react-dom": "ReactDOM",
-//         },
-//       },
-//     },
-//   },
-// });
-
-// vite.config.js
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
@@ -32,11 +6,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/index.client.js"),
       name: "@nosto/nosto-hydrogen",
-      // the proper extensions will be added
-      fileName: "nosto-hydrogen",
+      formats: ["es", "umd"],
+      fileName: "index.client",
     },
     rollupOptions: {
       external: ["react", "react-dom", "@shopify/hydrogen"],
